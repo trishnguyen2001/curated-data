2899e6d24beb5c3ac89a99957e9b94d0830487ce
Trish Nguyen <trishnguyen18@gmail.com>
2022-03-07 21:41:50 -0800

    Randomizer Created

--- sketch.js
+++ sketch.js
@@ -1,13 +1,104 @@
+let currentPic;
+let picArr = [];
+let side;
+let grid = [];
+let tRand;
+
+function preloadImgs() {
+  for (let i = 0; i < 55; i++) {
+    let filename = "dataset/love (" + i + ").jpg";
+    picArr[i] = loadImage(filename);
+    //console.log(filename);
+  }
+}
+
+function newPic(x, y) {
+  currentPic = picArr[int(random(picArr.length))];
+  currentPic.resize(side, 0);
+  image(currentPic, x, y);
+}
+
+function updateGrid() {
+  let cellRand = int(random(0, grid.length));
+  newPic(grid[cellRand].x, grid[cellRand].y);
+  tRand = randomTime();
+}
+
+function randomTime() {
+  return int(random(1, 10));
+}
+
+function preloadGrid() {
+  grid = [
+    {
+      pos: "topleft",
+      x: width * 0.08,
+      y: height * 0.08,
+    },
+    {
+      pos: "topmid",
+      x: width * 0.375,
+      y: height * 0.08,
+    },
+    {
+      pos: "topright",
+      x: width * 0.67,
+      y: height * 0.08,
+    },
+    {
+      pos: "midleft",
+      x: width * 0.08,
+      y: height * 0.375,
+    },
+    {
+      pos: "midmid",
+      x: width * 0.375,
+      y: height * 0.375,
+    },
+    {
+      pos: "midright",
+      x: width * 0.67,
+      y: height * 0.375,
+    },
+    {
+      pos: "botleft",
+      x: width * 0.08,
+      y: height * 0.67,
+    },
+    {
+      pos: "botmid",
+      x: width * 0.375,
+      y: height * 0.67,
+    },
+    {
+      pos: "botright",
+      x: width * 0.67,
+      y: height * 0.67,
+    },
+  ];
+}
+
 function setup() {
-  createCanvas(400, 400);
+  //canvas/drawing set up
+  createCanvas(windowHeight, windowHeight);
+  background(0, 0, 0);
+  imageMode(CORNER);
+  side = width * 0.25;
+  tRand = randomTime();
+
+  //preloads all images into picArr[]
+  preloadImgs();
+
+  //initializes grid
+  preloadGrid();
+  for (let i = 0; i < grid.length; i++) {
+    newPic(grid[i].x, grid[i].y);
+  }
 }
 
 function draw() {
-  background(220);
-  stroke(0, 0, 0);
-  strokeWeight(10);
-  ellipse(width * 0.5, height*0.5, width * 0.75, width * 0.75);
-  ellipse(width * 0.3, height * 0.4, width * 0.1, width * 0.1);
-  ellipse(width * 0.7, height * 0.4, width * 0.1, width * 0.1);
-  line(width* 0.4, height * 0.55, width * 0.6, height * 0.55);
+  if (frameCount % tRand == 0) {
+    updateGrid();
+  }
+  console.log(tRand);
 }
